7c6392eefaf3fe7b8a6020960f96d26e6dc04599
Filip Kalvach <filip.kalvach@gmail.com>
2025-11-13 17:44:38 +0100

    Add project files.

---
+++ ALM_Journal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ALM_Journal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
